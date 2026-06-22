99b30d3c4309b96e7a7ef9cf84c1ea0bdff45a78
Abdul Aziz <connect2abdulaziz@gmail.com>
2026-06-22 07:38:03 +0500

    ready for deployment

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,10 +1,11 @@
 const nextConfig = {
+  output: "standalone",
   reactStrictMode: true,
   images: {
     unoptimized: true,
     remotePatterns: [
       { protocol: "https", hostname: "lh3.googleusercontent.com", pathname: "/**" },
-      { protocol: "https", hostname: "res.cloudinary.com", pathname: "/**" },
+      { protocol: "https", hostname: "*.amazonaws.com", pathname: "/**" },
       { protocol: "https", hostname: "platform-lookaside.fbsbx.com", pathname: "/**" },
       { protocol: "https", hostname: "cdn-icons-png.flaticon.com", pathname: "/**" },
     ],
